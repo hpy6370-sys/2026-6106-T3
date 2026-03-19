e8ba017131130248d9e3556c491f0c90fddea73a
hpy6370-sys <hpy6370@gmail.com>
2026-03-19 10:59:24 +0000

    更新app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
-    return("<h2>Hi</h1>")
+    return(render_templates("index.html"))
 
 if __name__ == "__main__":
     app.run()
