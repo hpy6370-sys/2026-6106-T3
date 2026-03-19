4e559c6978adfee7e192708634305fea61c882ed
hpy6370-sys <hpy6370@gmail.com>
2026-03-19 11:29:12 +0000

    更新html

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
-    return(render_templates("index.html"))
+    return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    return(render_template("main.html"))
 
 if __name__ == "__main__":
-    app.run()
+        app.run()   
